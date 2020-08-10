e10f8adeecb1baa5fafb73803e21f379f79b3805
KSAhh <DSsooh578@likelion.org>
2020-08-10 23:09:10 +0900

    lesosn 4

--- page/models.py
+++ page/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Designer(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length = 50)
+    address = models.CharField(max_length = 255)
+    description = models.TextField()
+    # 디자이너 이름 표시 
+    def __str__(self):
+        return self.name
