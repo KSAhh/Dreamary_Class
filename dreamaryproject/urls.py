4334a4d907fd5f74dcef124612d334d693565031
KSAhh <DSsooh578@likelion.org>
2020-08-11 00:41:06 +0900

    lesson 5_Detail Page Added / home.html show profile Card

--- dreamaryproject/urls.py
+++ dreamaryproject/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', views.home, name = "home"),
 
     path('introduce/', views.introduce, name = "introduce"),
+    path('profile/<int:designer_id>', views.detail, name = "detail"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- page/views.py
+++ page/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Designer
 
 # Create your views here.
 def home(request):
+    designer = Designer.objects.all()
     # home에서 요청이 들어오면 home.html이 실행됨
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'designers' : designer})
 
 def introduce(request):
     return render(request, 'introduce.html')
     
+    # urls.py에 "designer_id" 라고 정의했기 때문에 "pk" 대신 "desginer_id"를 적음
+def detail(request, designer_id):
+    designer = get_object_or_404(Designer, pk = designer_id)
+    return render(request, 'detail.html', {'designer' : designer})
